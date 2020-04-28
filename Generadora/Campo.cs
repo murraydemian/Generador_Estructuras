5b59b9d66ca46586d4e28f9bed66bd024ea90a3a
murraydemian <murraydemian@gmail.com>
2020-04-27 23:47:12 -0300

    Se agrega Biblioteca de clases Generadora

---
+++ Generadora/Campo.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Generadora
+{
+    
+    public class Campo
+    {
+        private string _tipo;
+        private string _identificador;
+        private bool _referencia;
+        private bool _array;
+        private int _cantidad;
+        public Campo(string tipo, string identificador, bool referencia, bool array, int cantidad) {
+            this._tipo = tipo;
+            this._identificador = identificador;
+            this._referencia = referencia;
+            this._array = array;
+            this._cantidad = cantidad;
+        }
+        public string ParametroId
+        {
+            get
+            {
+                return this.Identificador.ToLower() + "_P";
+            }
+        }
+            
+        public string Tipo
+        {
+            get
+            {
+                return this._tipo;
+            }
+            set
+            {
+                if(value != null)
+                {
+                    this._tipo = value;
+                }
+            }
+        }
+        public string Identificador
+        {
+            get
+            {
+                return this._identificador;
+            }
+            set
+            {
+                if (value != null)
+                {
+                    this._identificador = value;
+                }
+            }
+        }
+        public bool Referencia
+        {
+            get
+            {
+                return this._referencia;
+            }
+            set
+            {
+                this._referencia = value;
+            }
+        }
+        public bool Array
+        {
+            get
+            {
+                return this._array;
+            }
+            set
+            {
+                this._array = value;
+            }
+        }
+        public int Cantidad
+        {
+            get
+            {
+                return this._cantidad;
+            }
+            set
+            {
+                this._cantidad = value;
+            }
+        }
+        public override string ToString()
+        {
+            string campo = "";
+            campo += this.Tipo;
+            if (this.Referencia)
+            {
+                campo += "*";
+            }
+            campo += " " + this.Identificador;
+            if (this.Array)
+            {
+                campo += "[" + this.Cantidad.ToString() + "]";
+            }
+            campo += ";";
+            return campo;
+        }
+        public string FirmaSet(string nombre)
+        {
+            string firma = "int " + nombre.ToLower() + "_Set" + this.Identificador 
+                + "(" + nombre + "* " + nombre.ToLower() + "_P, " + this.Tipo;
+            if (this.Referencia)
+            {
+                firma += "*";
+            }
+            if (this.Array)
+            {
+                firma += "[]";
+            }
+            firma += " " + this.Identificador.ToLower() + "_P);";
+            return firma;
+        }
+        public string FirmaGet(string nombre)
+        {
+            string firma = "int " + nombre.ToLower() + "_Get" + this.Identificador
+                + "(" + nombre + "* " + nombre.ToLower() + "_P, " + this.Tipo 
+                + "* " + this.Identificador + "_P);";
+            return firma;
+        }
+        public string FuncSet(string nombre)
+        {
+            string func = Regex.Replace(this.FirmaSet(nombre), ";", "");
+            func += "\n{\n\t" + "int ret = 0;\n\t";
+            if(this.Tipo == "char" && this.Array)
+            {
+                func += "if(" + nombre.ToLower() + "_P != NULL && " +
+                    this.Identificador + "_P != NULL)\n\t{\n\t\t" + 
+                    "strcpy(" + nombre.ToLower() + "_P->" + this.Identificador + 
+                    ", " + this.Identificador + "_P);\n\t\t";
+            }
+            else
+            {
+                func += "if(" + nombre.ToLower() + "_P != NULL)\n\t{\n\t\t" +
+                nombre.ToLower() + "_P->" + this.Identificador + " = " +
+                this.Identificador + "_P;\n\t\t";                     
+            }
+            func += "ret = 1;\n\t}\n\t" + "return ret;\n}";
+            return func;
+        }
+        public string FuncGet(string nombre)
+        {
+            string func = Regex.Replace(this.FirmaGet(nombre), ";", "");
+            func += "\n{\n\t" + "int ret = 0;\n\t";
+            if (this.Tipo == "char" && this.Array)
+            {
+                func += "if(" + nombre.ToLower() + "_P != NULL && " +
+                    this.Identificador + "_P != NULL)\n\t{\n\t\t" +
+                    "strcpy(" + this.Identificador + "_P, " + nombre.ToLower() +
+                    "_P->" + this.Identificador + ");\n\t\t";
+            }
+            else
+            {
+                func += "if(" + nombre.ToLower() + "_P != NULL)\n\t{\n\t\t" +
+                "*" + this.Identificador + "_P = " + this.Identificador + " = " +
+                nombre.ToLower() + "_P->" + this.Identificador + ";\n\t\t";
+            }
+            func += "ret = 1;\n\t}\n\t" + "return ret;\n}";            
+            return func;
+        }
+        public string SetCall(string nombre)
+        {
+            string call = nombre.ToLower() + "_Set" + this.Identificador
+                + "(" + nombre.ToLower() + "_P, " + this.Identificador + "_P);";
+            return call;
+        }
+    }
+}
---
+++ Generadora/Estructura.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Generadora
+{
+    public class Estructura
+    {
+        private string _nombre;
+        private List<Campo> _campos;
+
+        public Estructura()
+        {
+            this._campos = new List<Campo>();
+        }
+        public string Nombre
+        {
+            get
+            {
+                return this._nombre;
+            }
+            set
+            {
+                string nombre = Regex.Replace(value, @"\b(\w)", m => m.Value.ToUpper());
+                nombre = Regex.Replace(nombre, @"\s+", "");
+                try
+                {
+                    int.Parse(nombre.Substring(0, 1));
+                    nombre = "_" + nombre;
+                    this._nombre = nombre;
+                }
+                catch (Exception)
+                {
+                    this._nombre = nombre;
+                }
+            }
+        }
+        public List<Campo> Campos
+        {
+            get
+            {
+                return this._campos;
+            }            
+        }
+        public bool AgregarCampos(Campo cam)
+        {
+            bool seAgrego = false;
+            if (!VerificarRepetido(cam))
+            {
+                this._campos.Add(cam);
+                seAgrego = true;
+            }
+            return seAgrego;
+        }
+        private bool VerificarRepetido(Campo cam)
+        {
+            bool repetidos = false;
+            foreach(Campo item in this.Campos)
+            {
+                if(cam.Identificador == item.Identificador)
+                {
+                    repetidos = true;
+                    break;
+                }
+            }
+            return repetidos;
+        }
+        public override string ToString()
+        {
+            string struc = this.HeadString();
+            struc += "\n\n" + this.BuildString();
+            struc += "\n\n" + this.SetGetString();
+            return struc;
+        }
+        public string CFileString()
+        {
+            string completeC = this.BuildFunc() + "\n" + this.BuildFuncParam() + "\n";
+            foreach(Campo item in this.Campos)
+            {
+                completeC += item.FuncGet(this.Nombre) + "\n" +
+                    item.FuncSet(this.Nombre) + "\n";
+
+            }
+            return completeC;
+        }
+        public string HeadString()
+        {
+            string head = "typedef struct \n{\n";
+            foreach(Campo item in this.Campos)
+            {
+                head += item.ToString() + "\n";
+            }
+            head += "}" + this.Nombre + ";";
+            return head;
+        }
+        public string BuildString()
+        {
+            string builder = this.Nombre + "* " + this.Nombre.ToLower() + "_new";
+            builder += "();\n" + builder + "Parametros(";
+            foreach(Campo item in this.Campos)
+            {
+                builder += item.Tipo;
+                if (item.Referencia)
+                {
+                    builder += "*";
+                }
+                if (item.Array)
+                {
+                    builder += "[]";
+                }
+                builder += " " + item.ParametroId + ", ";
+            }
+            if(this.Campos.Count > 0)
+            {
+                builder = builder.Substring(0, builder.Length - 2);
+            }
+            builder += ");";
+            return builder;
+        }
+        public string SetGetString()
+        {
+            string campos = "";
+            foreach(Campo item in this.Campos)
+            {
+                campos += item.FirmaGet(this.Nombre) + "\n";
+                campos += item.FirmaSet(this.Nombre) + "\n\n";
+            }
+            return campos;
+        }
+        public string BuildFunc()
+        {
+            string func = this.Nombre + "* " + this.Nombre.ToLower() + "_new()\n{\n\t" +
+                "return (" + this.Nombre + ")malloc(sizeof(" + this.Nombre + "));\n}";
+            return func;
+        }
+        public string BuildFuncParam()
+        {
+            string func = this.Nombre + "* " + this.Nombre.ToLower() + "_newParametros(";
+            foreach(Campo item in this.Campos)
+            {
+                func += item.ParametroId + ", ";
+            }
+            func = func.Substring(0, func.Length - 2);
+            func += ")\n{\n\t" + this.Nombre + "* " + this.Nombre.ToLower() + "_P " +
+                " = " + this.Nombre.ToLower() + "_new();\n\t";
+            foreach(Campo item in this.Campos)
+            {
+                func += item.SetCall(this.Nombre) + "\n\t";
+            }
+            func += "return " + Nombre.ToLower() + "_P;\n}";
+            return func;
+        }
+        public void Guardar()
+        {
+            System.IO.File.WriteAllText(Environment.GetFolderPath(Environment.SpecialFolder.Desktop)
+                + "/" + this.Nombre + ".h", this.ToString());
+            System.IO.File.WriteAllText(Environment.GetFolderPath(Environment.SpecialFolder.Desktop)
+                + "/" + this.Nombre + ".c", this.CFileString());
+        }
+    }
+}
